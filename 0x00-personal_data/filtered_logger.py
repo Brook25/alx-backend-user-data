9693b23d15d9f7f868e2483971e64ade40291601
Brook25 <bekelebrook24@gmail.com>
2023-08-06 14:04:09 +0000

    add override format function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -12,3 +13,20 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f"{field}(.+?){separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.__fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.__fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
