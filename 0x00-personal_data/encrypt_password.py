b817d815a4316aabc58bb02702c68c764c33400c
Brook25 <bekelebrook24@gmail.com>
2023-08-07 16:00:10 +0000

    hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(pwd, salt)
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    hashed_password_2 = password.encode('utf-8')
+    return bcrypt.checkpw(hashed_password_2, hashed_password)
