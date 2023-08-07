3fb860dfe619642e660560d8f55fa378b4ca6aba
Brook25 <bekelebrook24@gmail.com>
2023-08-07 15:36:32 +0000

    hash password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""
-module contains funcs to Encrypt Passwords
+"""password encryption function
 """
 import bcrypt
 
 
-def hash_password(password: str) -> bytes:
-    """function returns a salted, hashed password,
-    """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """
-    function validates that the provided password matches
-    """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+def hash_password(password):
+    """returns a hashed password"""
+    pwd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(pwd, salt)
+    return hashed_pwd
