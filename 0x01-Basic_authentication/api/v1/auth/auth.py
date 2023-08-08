9208704521dccf489d5459411336d8621e0753cf
Brook25 <bekelebrook24@gmail.com>
2023-08-08 12:37:09 +0000

    forbidden abort 403

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,12 +5,14 @@ from typing import List, TypeVar
 from flask import request
 from api.v1.views.users import User
 
+
 class Auth:
     """template class for authentication system"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authorizaition for a certain path"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """get authorization header"""
         return None
