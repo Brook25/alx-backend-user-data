7be0c45166185a6c96946d12285a58a2b0c8a3da
Brook25 <bekelebrook24@gmail.com>
2023-08-08 15:07:30 +0000

    Basic authentication class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,11 +13,9 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = getenv('AUTH_TYPE')
-print(auth)
 if auth == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-    print(type(auth))
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
