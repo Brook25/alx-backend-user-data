39051f177184e62819c4c09a30cc397612828b59
Brook25 <bekelebrook24@gmail.com>
2023-08-08 12:04:39 +0000

    forbidden abort 403

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,13 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden error
+    """
+    return jsonify({"error": "Forbidden"})
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,14 @@ def unauthorized() -> None:
     abort(401)
 
 
+@app_views.route('forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """returns forbidden error
+    eventhough user is authenticated
+    """
+    abort(403)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
