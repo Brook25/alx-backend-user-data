e24168ec94e673e931ea0582d9a4d684dadcb136
Brook25 <bekelebrook24@gmail.com>
2023-08-08 15:32:03 +0000

    Basic authentication class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """Basic Auth
 """
-from auth import Auth
+from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Class for Basic Authentication"""
-    def extract_base64_authorization_header(
-        self, authorization_header: str
-    ) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """returns the authentication token"""
         if authorization_header and type(authorization_header) is str:
             if authorization_header[:6] == 'Basic ':
