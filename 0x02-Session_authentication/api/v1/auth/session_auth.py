69dc1984b940a03f8c1193bac69ebfe54d4026db
Brook25 <bekelebrook24@gmail.com>
2023-08-10 10:16:55 +0000

    current user based on session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,7 +4,7 @@
 from .auth import Auth
 from uuid import uuid4
 import os
-
+from models.user import User
 
 class SessionAuth(Auth):
     """Session Auth class"""
@@ -23,6 +23,15 @@ class SessionAuth(Auth):
             return self.user_id_by_session_id.get(session_id)
 
     def session_cookie(self, request=None):
-        """"""
+        """returns cookie value based on a session name"""
         if request:
             return request.cookies.get(os.getenv('SESSION_NAME'))
+    
+    def current_user(self, request=None):
+        """retrieve current user instance"""
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                user_id = self.user_id_for_session_id(session_id)
+                if user_id:
+                    return User.get(user_id)
